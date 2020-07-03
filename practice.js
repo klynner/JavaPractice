6a5d17b5e1bdbaaea69bc97d2a9fd038dce1323e
klynner <66446979+klynner@users.noreply.github.com>
2020-07-03 15:20:15 -0400

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,21 @@
+function check(){
+    //
+    var setting = document.getElementById("setting");
+    var mexport = document.getElementById("mexport");
+    var mimport = document.getElementById("mimport");
+
+    //        
+    var propername = txtBoy.value;
+    var mexport = mexport.value;
+    var mimport = mimport.value;
+   
+    
+    
+    //
+    var story = "Welcome to " + propername + ". There are many great things to do here.";
+story += "Come by our markets where you can get " + propername + "’s famous " + mexport + "." ;
+story += "Bring your own " + mimport + ".";
+    
+
+    output.innerHTML = story;
+}
